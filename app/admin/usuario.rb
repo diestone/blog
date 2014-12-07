64dc97f73335373b64977a45b6b3665bfb78a288
Rene Arreola <j.rene.arreola@gmail.com>
2014-12-06 19:03:00 -0800

    ActiveAdmin instalado

---
+++ app/admin/usuario.rb
@@ -0,0 +1,28 @@
+ActiveAdmin.register Usuario do
+index do
+#permit_params :nombre, :email
+
+column :nombre
+column "Correo-e", :email
+  
+
+
+
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+
+end
+end
+
